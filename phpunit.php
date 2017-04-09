0be8ebee4ce4e8fa86eddd048168c18345a83eb1
ssnepenthe <ssnepenthe@users.noreply.github.com>
2017-04-08 22:40:48 -0700

    Cleanup before major update.
    Editorconfig, composer deps, phpunit and readme.

--- phpunit.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// First we need to load the composer autoloader so we can use WP Mock
-require_once __DIR__ . '/vendor/autoload.php';
-
-// Now call the bootstrap method of WP Mock
-WP_Mock::activateStrictMode();
-WP_Mock::bootstrap();
